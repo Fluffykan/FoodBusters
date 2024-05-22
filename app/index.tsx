d6294b70f469b0b20760503a4e7672b50e1bdd8e
Kan Jun Hong <junhongkannot@gmail.com>
2024-05-22 11:28:20 +0800

    File update (profile page works here)
    not sure why I'm required to commit, no major changes
    might have been due to slight modifications yesterday night after previous commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native';
 import LoginPage from './pages/loginPage';
 import HomePage from './pages/homePage';
+import ProfilePage from './pages/profilePage';
 
 export default function index() {
   return (
     <View>
-      <LoginPage />
+      <ProfilePage />
     </View>
   );
 }
