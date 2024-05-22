aabaa789a885c05a2e5f441615dd75ef6e246855
Kan Jun Hong <junhongkannot@gmail.com>
2024-05-22 11:33:38 +0800

    Asset upload (png files) + ShopCondensedInfo.tsx
    Uploaded png files used and ShopCondensedInfo.tsx in order to close a new branch I created

---
+++ app/components/ShopCondensedInfo.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+export default function ShopCondensedInfo() {
+    return (
+        <View style={{flexDirection: 'row'}}>
+            <Image source={require('../assets/plateAndCutlery.png')} />
+            <View>
+                
+            </View>
+        </View>
+    )
+}
